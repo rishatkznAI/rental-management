070062b5e9253144224316ea5b34ddb3c45ae36e
rishatkznAI <habrishat@gmail.com>
2026-05-27 06:00:27 +0300

    fix(finance): wire company economics builder

--- server/server.js
+++ server/server.js
@@ -64,6 +64,7 @@ const {
   buildManagerReceivables,
   buildOverdueBuckets,
   buildFinanceReport,
+  buildCompanyEconomics,
   backfillPaymentAllocations,
 } = require('./lib/finance-core');
 const {
@@ -1355,6 +1356,7 @@ registerFinanceRoutes(apiRouter, {
   buildManagerReceivables,
   buildOverdueBuckets,
   buildFinanceReport,
+  buildCompanyEconomics,
   buildReceivables,
   normalizeAction,
   normalizePaymentPlan,
